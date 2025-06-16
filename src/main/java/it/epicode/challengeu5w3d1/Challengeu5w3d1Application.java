f09760db44801771cb21779de19f4c831d5e1165
Pierattilio-correale <pierattiliocorreale@yahoo.it>
2025-06-16 14:29:24 +0200

    Initial commit

---
+++ src/main/java/it/epicode/challengeu5w3d1/Challengeu5w3d1Application.java
@@ -0,0 +1,13 @@
+package it.epicode.challengeu5w3d1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challengeu5w3d1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Challengeu5w3d1Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/challengeu5w3d1/Challengeu5w3d1ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.challengeu5w3d1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Challengeu5w3d1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
